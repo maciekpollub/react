316a77aaaabf64d36f6b0443271f1628ddaaf6ff
Maciej Ziemba <maciek.ziemba@gmail.com>
2017-11-18 09:30:55 +0100

    My first app in React added

---
+++ react-component/src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => {
+    return (
+
+        <div>
+            <h1>React exercises</h1>
+
+            Hello world
+
+            <p>paragraph added...</p>
+        </div>
+
+    );
+};
+
+export default App;
