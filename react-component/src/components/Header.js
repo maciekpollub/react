97335ed7ba7e78241f0a5edd94d045fee4d9347b
Maciej Ziemba <maciek.ziemba@gmail.com>
2017-11-18 11:07:02 +0100

    Styling and components importing  rendered..

--- react-component/src/components/Header.js
+++ react-component/src/components/Header.js
@@ -1,10 +1,22 @@
 import React from 'react' ;
 import Menu from './Menu';//importujemy  Menu do Headera - zgodnie ze strukturą strony...
+import obrazek from '../logo.svg';//importtowanie obrazka do pliku odpowiadającego elementowi struktury Dom w którym chcemy mieć obrazek
+//import './logo.css';
+
+
+//teraz wprowadzamy obiekt logoStyle, który inline'owo ostyluje nam obrazek
+const logoStyle = {
+  width: '100px',
+  height: '120px',
+    fontSize: '30px'
+};
 
 const Header  = () => {
     return(
-        <div>
+        <div className = 'header'>
             To jest header!
+            <img style = {logoStyle}
+                 src = {obrazek}/>;
             <Menu/>
         </div>
     )
--- react-component/src/components/Menu.js
+++ react-component/src/components/Menu.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const Menu = () => {
-    const menuElements = [
-       'Home', 'Profile', 'Dashboard'
-    ].map((element, index) => {
-        return <li key = {index}>{element}</li>
-    }
-    );
+    const menuElements = [//tablica z elementami menu...
+            'Home',
+            'Profile',
+            'Dashboard'
+        ].map((element, index) => {//metoda map jako parametr pobiera funkcję dwuparametrową: aktualnie przetwarzany elem tablicy i index tablicy
+            return <li key = {index}>{element}</li>//poniewaź tagi li się powtarzają, to muszą mieć unikalne klucze (jak identyfikatory)
+            }
+        );
     return (
         <div>
             <ul>
