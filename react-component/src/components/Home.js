c2673058feaf0b00abdd95ccb6aec4a3f60e8652
Maciej Ziemba <maciek.ziemba@gmail.com>
2017-11-19 09:13:02 +0100

    Grid columns added...

--- react-component/src/components/Home.js
+++ react-component/src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Button} from 'react-bootstrap';
+import {Grid, Row, Col} from 'react-bootstrap';//kazdy wykorzystywany komponent rb musi zostać zaimportowany
 
 
 const Home = () => {
@@ -9,7 +10,14 @@ const Home = () => {
     return (
         <div>
             /*przycisk wywołujący funkcję handleClick*/
-            <Button bsStyle = "primary" onClick = {handleClick}>Button</Button>
+            <Button bsStyle = "primary" bsSize = "small" onClick = {handleClick}>Button</Button>
+            /*tworzenie kolumn w react-bootstrapie...*/
+            <Grid>
+                <Row className='show-grid'>
+                    <Col xs={12} md={8}>Column no 1</Col>
+                    <Col xs={12} md={4}>Column no 2</Col>
+                </Row>
+            </Grid>
         </div>
     );
 
