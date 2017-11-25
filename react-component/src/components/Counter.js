4a8efbe12e4b2211ff1a56914887413469edcb76
Maciej Ziemba <maciek.ziemba@gmail.com>
2017-11-25 12:17:18 +0100

    exercises done

--- react-component/src/components/Counter.js
+++ react-component/src/components/Counter.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Counter extends React.Component {
     constructor(myAwesomeProps){
         super(myAwesomeProps);
-        this.add = this.add.bind(this);//musimy zbindować by this odnosił sie do ..
+        this.add = this.add.bind(this);//musimy zbindować by this odnosił sie do constructora. Można by zamiast tego użyc funkcji strzałkowej w add i subgt
         this.sub = this.sub.bind(this);
         this.state = {
             count: 0
@@ -22,6 +22,25 @@ class Counter extends React.Component {
             count: this.state.count -1
         });
     }
+    componentWillMount() {
+        console.log('jestem w will mount');
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        if (nextProps.name === 'Warszawa'){
+            return false;//nie wyrenderuje sie jeżli wpadnie Warszawa...
+        }
+        if (nextState.count === 100){
+            return false;//jeśli uzytkownik wyklika 100 razy to zblokujemy go...
+        }
+        return true;
+    }
+
+    componentWillReceiveProps(nextProps){
+        //tutaj można robić operacje na stanie
+        this.props.name //Lublin
+        nextProps.name//W-wa
+    }
 
     render(){
         return <div>
@@ -29,6 +48,13 @@ class Counter extends React.Component {
             <button onClick={this.add}>+</button>
             <button onClick={this.sub}>-</button>
         </div>
+    }
+
+    componentDidMount() {
+        console.log('jestem w did mount');
+    }
+    componentWillUnmount() {
+        console.log('jestem w unmount');
     }
 }
 export default Counter;
